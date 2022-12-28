391bd67988f16b6a86ee607c56ace3b9c9d12854
ginaesps <respinosa7@ucol.mx>
2022-12-28 01:20:21 -0600

    clean code and restructure flags input

--- ledger.py
+++ ledger.py
@@ -3,28 +3,6 @@ import argparse
 
 transactions = [];
 
-def LGregister(): #accounts, options
-    #print(getTransactions(options.F))
-    print("reg funct")
-
-def LGbalance(): #accounts, options
-    #print(getTransactions(options.F))
-    print("bal funct")
-    print(parser.parse_args())
-
-def LGprint():
-    #print(getTransactions(options.F))
-    print("print func")
-
-
-def getTransactions():
-    with open(filename) as file:
-        while (line := file.readline().rstrip()):
-            print(line)
-    # cada que se acabe una transaction        
-    transactions.push(file)        
-
-
 #program description
 arg_desc = '''\
         Let's keep track of your finance!
@@ -33,9 +11,7 @@ arg_desc = '''\
             of a Ledger CLI with Python argparse!
         '''
 
-#ArgumentParser adds arguments and parse them
-#parser will be set with parse_args()
-    #this method will be wraped with vars() to avoid errors
+#ArgumentParser adds arguments and parses them
 parser = argparse.ArgumentParser(prog = 'ledger',
                                 formatter_class = argparse.RawDescriptionHelpFormatter,
                                 description= arg_desc)
@@ -58,32 +34,7 @@ parser.add_argument("command",
                     choices=['balance', 'bal', 'register', 'reg', 'print'],
                     help='Select a command to execute')
 
-"""#register command
-parser.add_argument('-reg', '--register', 
-                    help='prints all the transactions and the running total',
-                    action=LGregister())      # option that takes a value
-
-#balance command
-parser.add_argument('-bal', '--balance', 
-                    help='prints the balances of your journal accounts',
-                    action=LGbalance())      # option that takes a value
-
-#print command
-parser.add_argument('-p', '--print', 
-                    help='prints transactions in a textual format parsed by ledger',
-                    action=LGprint())      # option that takes a value"""
- 
-args = vars(parser.parse_args())
+#Parse the arguments given as inputs 
+args = parser.parse_args()
 print(args)
-print("hasdofaisdf")
-
-#if args["-bal"]:
-#    print(args)
-
-#options
-parser.add_argument('--price-db',
-                    help='The fiel for the prices database')
-parser.add_argument('--file', '-f <string....>',
-                    help='The fiel for the prices database')
-parser.add_argument('-s', '--sort',
-                    help='Sort a report')
+print("debug")
