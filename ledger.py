f81b49f7e966efeb6535bdae9eab51ce6cec13ca
ginaesps <respinosa7@ucol.mx>
2023-01-01 22:13:43 -0600

    Transaction class creation

--- ledger.py
+++ ledger.py
@@ -4,6 +4,51 @@ import argparse
 data = []
 
 """
+----------- Transaction class() -----------
+The constructor includes date, comment, account1, amount1, account2 and amount2.
+"""
+class Transaction():
+    def __init()__(self, date, comment, account1, amount1, account2, amount2=None):
+        self.date = date
+        self.comment = comment
+        self.account1 = account1
+        self.account2 = account2
+
+        #if amount2=None, amount1 is acommodity or amount depending if it has a space.
+        #either case, the amounts are lists with symbol/currency and value.
+        if not amount2:
+            if (' ' in amount1): #commodity
+                amount = amount1.split(' ')
+                self.amount1 = [amount[1], float(amount[0])]
+                self.amount2 = [amount[1], float(amount[0])* -1 ]
+            else: #amount
+                if ('-' in amount1):
+                    amount1 = amount1.replace('-','')
+                    self.amount1 = [amount1[0], float(amount1[1:])* -1]
+                    self.amount2 = [amount1[0], float(amount1[1:])]
+                else:
+                    self.amount1 = [amount1[0], float(amount1[1:])]    
+                    self.amount2 = [amount1[0], float(amount1[1:])* -1]
+
+        else:
+            if ('-' in amount1):
+                amount1 = amount1.replace('-', '')
+                self.amount1 = [amount1[0], float(amount1[1:])* -1]
+            else:
+                self.amount1 = [amount1[0], float(amount1[1:])]
+
+        if (' ' in amount2):
+            amount = amount2.split(' ')
+            self.amount2 = [amount[1], float(amount[0])]
+        else: #Am2 amount
+            if('-' in amount2):
+                amount2 = amount2.replace('-','')
+                self.amount2 = [amount2[0], float(amount2[1:])* -1]
+            else:
+                self.amount2 = [amount2[0], float(amount2[1:])]
+
+
+"""
 ----------- readFile function() -----------
 reads a file and stores its lines in the data list.
 the with statement opens the file and reads it line by line with for and readlines()
